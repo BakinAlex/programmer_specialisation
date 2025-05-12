dce0922e80b11e9fffae6c8f3f3a697d6fbfec12
Aleksey <bakinlexa@mail.ru>
2025-05-12 22:30:58 +0300

    Completed the lesson homework

---
+++ python_introduction/lesson_1.py
@@ -0,0 +1,41 @@
+#Task_1
+a = 8
+b = 10
+c = 12
+d = 18
+
+divisible = (-3 + a**2) * b - 2**3
+
+divider = c - 2 * d
+
+square = divisible / divider
+
+print(square)
+
+#Task_2
+#user = int(input('Введите кол-во минут: '))
+#hour = user // 60
+#minutes = user % 60
+#print(f'{hour=}, {minutes=}')
+
+#Task_3
+# user = int(input('Введите номер билета: '))
+# first_num = user // 100000
+# second_num = user % 100000 // 10000
+# third_num = user % 100000 % 10000 // 1000
+# fourth_num = user % 100000 % 10000 % 1000 // 100
+# fifth_num = user % 100000 % 10000 % 1000 % 100 // 10
+# sixth_num = user % 100000 % 10000 % 1000 % 100 % 10
+# first_part = first_num + second_num + third_num
+# second_part = fourth_num + fifth_num + sixth_num
+
+# if first_part == second_part:
+#     print('Yes!')
+# else:
+#     print('No!')
+
+#Task_4
+a = int(input('Введите данные первого катета: '))
+b = int(input('Введите данные второго катета: '))
+square = (1 / 2) * a * b
+print(f'{square=}')
